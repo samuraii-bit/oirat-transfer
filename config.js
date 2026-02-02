cebd587db787e1b26c6aeaea595da5aaf60eac95
Timur Muzraev <muzraevt@mail.ru>
2026-02-02 09:35:38 +0300

    script address edited

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxERySveGT9KqBqffMr2z-cSjGpIvCHp7sa5gYxGrErKYb52howHsgoO3nnvc2tAUde/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbzUffcf-vu-qCK24BU68CBJRjnSWWFNU8TxB6_sSgveKam6l6dI6AYeSfWE_dHyN00h/exec"
 };
